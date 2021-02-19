4a8259dea1d9f8d340dbae84f7a9548a15e08d59
vtrpza <vhnpouza@gmail.com>
2021-02-19 14:43:00 -0300

    script atualizad

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,12 @@ import { AppProps } from 'next/app'
 import Head from 'next/head'
 
 import GlobalStyles from 'styles/global'
-           
-function App({ Component, pageProps }:                 AppProps) {
-  return (        
+
+function App({ Component, pageProps }: AppProps) {
+  return (
     <>
       <Head>
-        <title>Boilerplate</fasfaaftitle>
+        <title>Boilerplate</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <meta
